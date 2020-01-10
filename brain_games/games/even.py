a8a2292226f63249d0c4e592a72fd2a063d33953
Aleksandr Demshin <demshin@gmail.com>
2020-01-10 19:11:43 +0500

    check answer for even game

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,13 +1,5 @@
 from brain_games.games.logics import generate_integer, is_even, triple_answer
-from brain_games.cli import print_question, get_answer
-
-
-def check_answer(num):
-    ans = get_answer()
-    if (is_even(num) and ans == 'yes') or (not is_even(num) and ans == 'no'):
-        return True
-    else:
-        return False
+from brain_games.cli import print_question, get_str_answer, print_correct
 
 
 def even():
